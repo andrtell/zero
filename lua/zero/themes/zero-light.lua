a6db887d38d19604cc5d240ed7b9cd9f0d3768df
andrtell <116459057+andrtell@users.noreply.github.com>
2026-03-15 18:51:28 +0100

    updates

--- lua/zero/themes/zero-light.lua
+++ lua/zero/themes/zero-light.lua
@@ -4,30 +4,29 @@ local red     =   '#7e1e00'
 local green   =   '#025603'
 local blue    =   '#05349c'
 local purple  =   '#6b0c6b'
-local grey    = { '#98989a',
-                  '#e5e4e2',
-                  '#ededec',
-                  '#e7e6e4',
-                  '#e5e5e5' }
+local gray    = { comment = '#98989a',
+                  status  = '#e5e4e2',
+                  search  = '#e7e6e4',
+                  visual  = '#ededec' }
 
 local Normal      = { fg = black,
-                      bg = white   }
-local Invisible   = { fg = white   }
-local Visual      = { bg = grey[3] }
-local Search      = { bg = grey[4] }
-local MenuSelect  = { bg = grey[2] }
-local ScrollBar   = { bg = grey[4] }
-local Status      = { bg = grey[2] }
-local Directory   = { fg = blue    }
-local Title       = { fg = blue    }
-local Error       = { fg = red     }
-local Warn        = { fg = purple  }
-local Hint        = { fg = blue    }
-local Keyword     = { fg = red     }
-local Number      = { fg = blue    }
-local String      = { fg = green   }
-local Boolean     = { fg = purple  }
-local Comment     = { fg = grey[1] }
+                      bg = white        }
+local Invisible   = { fg = white        }
+local Visual      = { bg = gray.visual  }
+local Search      = { bg = gray.search  }
+local MenuSelect  = { bg = gray.status  }
+local ScrollBar   = { bg = gray.search  }
+local Status      = { bg = gray.status  }
+local Directory   = { fg = blue         }
+local Title       = { fg = blue         }
+local Error       = { fg = red          }
+local Warn        = { fg = purple       }
+local Hint        = { fg = blue         }
+local Keyword     = { fg = red          }
+local Number      = { fg = blue         }
+local String      = { fg = green        }
+local Boolean     = { fg = purple       }
+local Comment     = { fg = gray.comment }
 
 return {
   Normal,     'Normal',
--- nvim.sh
+++ nvim.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nvim --cmd 'set rtp+=./' 
+nvim --cmd "set rtp+=$(pwd)/"
