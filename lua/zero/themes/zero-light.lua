36bf1859e0ac4ff06f4812876b91006354da2606
andrtell <116459057+andrtell@users.noreply.github.com>
2026-03-15 17:38:01 +0100

    cleanup theme

--- lua/zero/themes/zero-light.lua
+++ lua/zero/themes/zero-light.lua
@@ -1,75 +1,57 @@
-local c = {
-  white  = { '#fafafa'  },
-  black  = { '#000000'  },
-  grey   = { '#98989a',
-             '#e5e4e2',
-             '#ededec',
-             '#e7e6e4',
-             '#e5e5e5', },
-  red    = { '#7e1e00'  },
-  green  = { '#025603',
-             '#025002'  },
-  blue   = { '#05349c'  },
-  purple = { '#6b0c6b'  },
-}
+local black   =   '#000000'
+local white   =   '#fafafa'
+local red     =   '#7e1e00'
+local green   =   '#025603'
+local blue    =   '#05349c'
+local purple  =   '#6b0c6b'
+local grey    = { '#98989a',
+                  '#e5e4e2',
+                  '#ededec',
+                  '#e7e6e4',
+                  '#e5e5e5' }
 
-local h = {
-  Normal     = { fg = c.black[1],
-                 bg = c.white[1]  },
-  Invisible  = { fg = c.white[1]  },
-
-  Visual     = { bg = c.grey[3]   },
-  Search     = { bg = c.grey[4],  },
-  MenuSelect = { bg = c.grey[2]   },
-  ScrollBar  = { bg = c.grey[4]   },
-
-  Status     = { bg = c.grey[2]   },
-
-  Directory  = { fg = c.blue[1]   },
-
-  Title      = { fg = c.blue[1]   },
-
-  Error      = { fg = c.red[1]    },
-  Warn       = { fg = c.purple[1] },
-  Hint       = { fg = c.blue[1]   },
-
-  Keyword    = { fg = c.red[1]    },
-  Number     = { fg = c.blue[1]   },
-  String     = { fg = c.green[1]  },
-  Boolean    = { fg = c.purple[1] },
-  Comment    = { fg = c.grey[1]   },
-}
+local Normal      = { fg = black,
+                      bg = white   }
+local Invisible   = { fg = white   }
+local Visual      = { bg = grey[3] }
+local Search      = { bg = grey[4] }
+local MenuSelect  = { bg = grey[2] }
+local ScrollBar   = { bg = grey[4] }
+local Status      = { bg = grey[2] }
+local Directory   = { fg = blue    }
+local Title       = { fg = blue    }
+local Error       = { fg = red     }
+local Warn        = { fg = purple  }
+local Hint        = { fg = blue    }
+local Keyword     = { fg = red     }
+local Number      = { fg = blue    }
+local String      = { fg = green   }
+local Boolean     = { fg = purple  }
+local Comment     = { fg = grey[1] }
 
 return {
-  h.Normal,      'Normal',
-  h.Invisible,   'NonText',
-
-  h.Visual,     'Visual',
-                'LeapLabel',
-  h.Search,     'Search',
-                'CurSearch',
-                'IncSearch',
-
-  h.Status,     'StatusLine',
-
-  h.MenuSelect, 'PMenuSelect',
-                'BlinkCmpMenuSelection',
-  h.ScrollBar,  'BlinkCmpScrollBarThumb',
-
-  h.Directory,  'Directory',
-
-  h.Title,      'Title',
-
-  h.Hint,       'DiagnosticHint',
-  h.Warn,       'DiagnosticWarn',
-  h.Error,      'DiagnosticError',
-
-  h.Comment,    'Comment',
-  h.Boolean,    'Boolean',
-  h.Number,     'Number', 'Float',
-  h.String,     'String',
-  h.Keyword,    'Keyword', 'Statement', '@keyword.function',
-                '@keyword.repeat', '@keyword.conditional',
-                '@keyword.type',
+  Normal,     'Normal',
+  Invisible,  'NonText',
+  Visual,     'Visual',
+              'LeapLabel',
+  Search,     'Search',
+              'CurSearch',
+              'IncSearch',
+  Status,     'StatusLine',
+  MenuSelect, 'PMenuSelect',
+              'BlinkCmpMenuSelection',
+  ScrollBar,  'BlinkCmpScrollBarThumb',
+  Directory,  'Directory',
+  Title,      'Title',
+  Hint,       'DiagnosticHint',
+  Warn,       'DiagnosticWarn',
+  Error,      'DiagnosticError',
+  Comment,    'Comment',
+  Boolean,    'Boolean',
+  Number,     'Number', 'Float',
+  String,     'String',
+  Keyword,    'Keyword', 'Statement', '@keyword.function',
+              '@keyword.repeat', '@keyword.conditional',
+              '@keyword.type',
 }
 
