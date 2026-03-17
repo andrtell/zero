91519bef70ac4ef2ac905ea2483f96aafb39076f
andrtell <116459057+andrtell@users.noreply.github.com>
2026-03-17 07:43:56 +0100

    cleanup theme

--- lua/zero/themes/rgb-light.lua
+++ lua/zero/themes/rgb-light.lua
@@ -1,65 +1,62 @@
---// RGB Light //--
+-- Colors
 
---/ COLORS /--
+local black   = { fg    = '#010101'   }
+local white   = { bg    = '#fafafa'   }
+local red     = { fg    = '#7c1f00'   }
+local green   = { fg    = '#045404'   }
+local blue    = { fg    = '#034792'   }
+local purple  = { fg    = '#6a156a'   }
+local yellow  = { bg    = '#ededd9'   }
+local gray    = { fg    = '#969696',
+                  bg    = '#eceeee',  }
 
-local black   =   '#010101'
-local white   =   '#fafafa'
-local red     =   '#7c1f00'
-local green   =   '#045404'
-local blue    =   '#034792'
-local purple  =   '#6a156a'
-local gray    = { comment = '#979797',
-                  visual  = '#eceeee',
-                  search  = '#ededd9',
-                  status  = '#eaebec', }
+-- Opts
 
---/ OPTS /--
+local Normal      = { fg = black.fg,
+                      bg = white.bg   }
+local NonText     = { fg = gray.fg    }
+local Visual      = { bg = gray.bg    }
+local Search      = { bg = yellow.bg  }
+local Red         = { fg = red.fg     }
+local Green       = { fg = green.fg   }
+local Blue        = { fg = blue.fg    }
+local Purple      = { fg = purple.fg  }
+local Gray        = { fg = gray.fg    }
 
-local Normal      = { fg = black,
-                      bg = white        }
-local Invisible   = { fg = white        }
-local Visual      = { bg = gray.visual  }
-local Search      = { bg = gray.search  }
-local MenuSelect  = { bg = gray.status  }
-local ScrollBar   = { bg = gray.search  }
-local Status      = { bg = gray.status  }
-local Directory   = { fg = blue         }
-local Title       = { fg = blue         }
-local Error       = { fg = red          }
-local Warn        = { fg = purple       }
-local Hint        = { fg = blue         }
-local Keyword     = { fg = red          }
-local Number      = { fg = blue         }
-local String      = { fg = green        }
-local Boolean     = { fg = purple       }
-local Comment     = { fg = gray.comment }
-local Type        = { fg = blue         }
-
---/ THEME /--
+-- Theme
 
 return {
   Normal,     'Normal',
-  Invisible,  'NonText',
-  Visual,     'Visual',
-              'LeapLabel',
+  NonText,    'NonText',
   Search,     'Search',
               'CurSearch',
               'IncSearch',
-  Status,     'StatusLine',
-  MenuSelect, 'PMenuSelect',
-              'BlinkCmpMenuSelection',
-  ScrollBar,  'BlinkCmpScrollBarThumb',
-  Directory,  'Directory',
-  Title,      'Title',
-  Hint,       'DiagnosticHint',
-  Warn,       'DiagnosticWarn',
-  Error,      'DiagnosticError',
-  Comment,    'Comment',
-  Boolean,    'Boolean',
-  Number,     'Number', 'Float',
-  String,     'String',
-  Type,       'Type',
-  Keyword,    'Keyword', 'Statement', '@keyword.function',
-              '@keyword.repeat', '@keyword.conditional',
+  Visual,     'Visual',
+              'StatusLine',
+              'PMenuSelect',
+  Blue,       'Directory',
+              'Title',
+              -- Diagnostic
+  Red,        'DiagnosticError',
+  Blue,       'DiagnosticHint',
+  Purple,     'DiagnosticWarn',
+              -- Syntax
+  Red,        'Keyword',
+              'Statement',
+              '@keyword.function',
+              '@keyword.repeat',
+              '@keyword.conditional',
               '@keyword.type',
+  Green,      'String',
+  Blue,       'Number',
+              'Float',
+              'Type',
+  Purple,     'Boolean',
+              'Special',
+  Gray,       'Comment',
+              -- Blink
+  Visual,     'BlinkCmpMenuSelection',
+              'BlinkCmpScrollBarThumb',
+              -- Leap
+  Visual,     'LeapLabel',
 }
