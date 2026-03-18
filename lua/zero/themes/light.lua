b65c16ba3fe7b3382ebc148e861dc7d1f372fef7
andrtell <116459057+andrtell@users.noreply.github.com>
2026-03-18 14:34:16 +0100

    tweak

--- lua/zero/themes/light.lua
+++ lua/zero/themes/light.lua
@@ -2,7 +2,7 @@
 
 local black   = { fg = '#030303'  }
 local white   = { bg = '#fafafa'  }
-local red     = { fg = '#7f1e1e'  }
+local red     = { fg = '#7f3535'  }
 local green   = { fg = '#045504'  }
 local blue    = { fg = '#014495'  }
 local purple  = { fg = '#6e2275'  }
