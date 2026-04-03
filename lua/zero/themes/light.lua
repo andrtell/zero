1c8b573e1407c5bdc008cc22262d857c0f86ac33
andrtell <116459057+andrtell@users.noreply.github.com>
2026-04-03 13:13:15 +0200

    tweak

--- lua/zero/themes/light.lua
+++ lua/zero/themes/light.lua
@@ -66,7 +66,7 @@ local ih = {
   l.Green,     'String',
   l.Blue,      'Number',
                'Float',
-  l.Purple,    'Boolean',
+  l.Cyan,      'Boolean',
   l.GrayB,     'BlinkCmpMenuSelection',
                'BlinkCmpScrollBarThumb',
   l.YellowB,   'LeapLabel',
